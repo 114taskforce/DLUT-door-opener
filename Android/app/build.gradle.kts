3d66c066bca13dbf9c4637cef2a0b9bcacfad074
114taskforce <181956358+114taskforce@users.noreply.github.com>
2026-09-05 01:55:48 +0800

    Android: v1.1 (versionCode 2)

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.dlut.dooropener"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
